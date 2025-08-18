eaa97cede2fd222388a2cbfc1ea0a6f954d825ed
bellyparagon <almeidadesouzaisabelly27@gmail.com>
2025-08-17 20:02:09 -0400

    Update Projeto.c

--- Projeto.c
+++ Projeto.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main (){ 
+    float nota1, nota2, nota3;
+    float
+    média;
+    printf("*** Programa de Cáculo de Média ***\n");
+  
+    printf("Digite sua primeira nota\n");
+    scanf("%f", &nota1);
+
+    printf("Digite sua segunda nota\n"); 
+    scanf("%f", &nota2);
+
+    printf("Digite sua terceira nota\n");
+    scanf("%f", &nota3);
+
+    média = (nota1 + nota2 + nota3) / 3;
+
+    printf("A média é: %.2f", média);
+
+    return 0;
+}
 
